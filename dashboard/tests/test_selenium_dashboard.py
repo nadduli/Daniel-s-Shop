e3ed79d051fad215ae4de05e22815d1e99431217
nadduli <naddulidaniel94@gmail.com>
2022-07-08 15:24:16 +0300

    added db fixtures

--- dashboard/tests/test_selenium_dashboard.py
+++ dashboard/tests/test_selenium_dashboard.py
@@ -1,2 +1,7 @@
-def test_example():
-    assert "Hello" == "Hello"
+import pytest
+from selenium.webdriver.common.keys import Keys
+
+@pytest.mark.selenium
+def test_dashboard_admin_login(live_server, chrome_browser_instance):
+     browser = chrome_browser_instance
+     browser.get(("%s%s" %(live_server.url, "/admin/login")))
